f58dbe0c17803c9ed4188e9f16ed3e12757b6d88
Yummy275 <htgonzalez94@yahoo.com>
2021-03-11 10:23:50 -0700

    cleaned up placing page

---
+++ src/components/PlacingShipsNames.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useRef } from 'react';
+
+const classes = {
+    shipName: (placingShip, shipName) =>
+        `mx-1 ${placingShip.name === shipName ? 'text-red-700' : ''}`,
+};
+
+const PlacingShipsNames = ({ placingShip }) => {
+    const shipNameOne = useRef();
+    const shipNameTwo = useRef();
+    const shipNameThr = useRef();
+    const shipNameFour = useRef();
+    const shipNameFive = useRef();
+
+    //resets all the words to not be striked out
+    const resetStrikeOutText = () => {
+        shipNameOne.current.style.textDecoration = 'none';
+        shipNameTwo.current.style.textDecoration = 'none';
+        shipNameThr.current.style.textDecoration = 'none';
+        shipNameFour.current.style.textDecoration = 'none';
+        shipNameFive.current.style.textDecoration = 'none';
+    };
+    useEffect(() => {
+        if (placingShip.name === 'Battleship') {
+            shipNameOne.current.style.textDecoration = 'line-through';
+        } else if (placingShip.name === 'Cruiser') {
+            shipNameTwo.current.style.textDecoration = 'line-through';
+        } else if (placingShip.name === 'Submarine') {
+            shipNameThr.current.style.textDecoration = 'line-through';
+        } else if (placingShip.name === 'Destroyer') {
+            shipNameFour.current.style.textDecoration = 'line-through';
+        } else if (placingShip === 'done') {
+            shipNameFive.current.style.textDecoration = 'line-through';
+        }
+
+        return () => {
+            if (placingShip === 'done') {
+                resetStrikeOutText();
+            }
+        };
+    });
+
+    return (
+        <>
+            <div
+                ref={shipNameOne}
+                className={classes.shipName(placingShip, 'Carrier')}
+            >
+                Carrier
+            </div>
+            <div
+                ref={shipNameTwo}
+                className={classes.shipName(placingShip, 'Battleship')}
+            >
+                Battleship
+            </div>
+            <div
+                ref={shipNameThr}
+                className={classes.shipName(placingShip, 'Cruiser')}
+            >
+                Cruiser
+            </div>
+            <div
+                ref={shipNameFour}
+                className={classes.shipName(placingShip, 'Submarine')}
+            >
+                Submarine
+            </div>
+            <div
+                ref={shipNameFive}
+                className={classes.shipName(placingShip, 'Destroyer')}
+            >
+                Destroyer
+            </div>
+        </>
+    );
+};
+
+export default PlacingShipsNames;
--- src/pages/PlacingPage.js
+++ src/pages/PlacingPage.js
@@ -1,9 +1,26 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import BoardGrid from '../components/BoardGrid';
 import StdButton from '../components/StdButton';
+import PlacingShipsNames from '../components/PlacingShipsNames';
 import skull from '../images/skull.png';
 import scroll from '../images/home-scroll.png';
 import rand from '../util/random';
+
+const classes = {
+    container: `h-screen bg-center`,
+    headerBar: `text-center text-red-700 text-3xl pb-1 bg-gray-300 bg-opacity-90`,
+    boardGridContainer: `h-4/6 w-5/6 mt-3 mx-auto bg-water-tile my-grid-border`,
+    doneButton: (placingShip) =>
+        `${
+            placingShip === 'done' ? 'flex' : 'hidden'
+        } justify-center text-red-700`,
+    axisButtonsContainer: (placingShip) =>
+        `${
+            placingShip !== 'done' ? 'flex' : 'hidden'
+        } justify-evenly text-red-700`,
+    shipNamesContainer: `relative flex justify-center items-center`,
+    shipNames: `absolute w-44 sm:w-64 text-sm sm:text-lg flex flex-wrap justify-around`,
+};
 
 const PlacingPage = ({ player, setViewPage, vsCpu }) => {
     useEffect(() => {
@@ -16,42 +33,16 @@ const PlacingPage = ({ player, setViewPage, vsCpu }) => {
     const [placingShip, setPlacingShip] = useState(player.ships['carrier']);
     const [axis, setAxis] = useState('X');
 
-    //used to strikeout ship name when done placing ship
-    const shipNameOne = useRef();
-    const shipNameTwo = useRef();
-    const shipNameThr = useRef();
-    const shipNameFour = useRef();
-    const shipNameFive = useRef();
-
-    //strikes out words in inputed ele
-    const strikeOutText = (ele) => {
-        ele.current.style.textDecoration = 'line-through';
-    };
-
-    //resets all the words to not be striked out
-    const resetStrikeOutText = () => {
-        shipNameOne.current.style.textDecoration = 'none';
-        shipNameTwo.current.style.textDecoration = 'none';
-        shipNameThr.current.style.textDecoration = 'none';
-        shipNameFour.current.style.textDecoration = 'none';
-        shipNameFive.current.style.textDecoration = 'none';
-    };
-
     const updatePlacingShip = () => {
         if (placingShip.name === 'Carrier') {
-            strikeOutText(shipNameOne);
             setPlacingShip(player.ships['battleship']);
         } else if (placingShip.name === 'Battleship') {
-            strikeOutText(shipNameTwo);
             setPlacingShip(player.ships['cruiser']);
         } else if (placingShip.name === 'Cruiser') {
-            strikeOutText(shipNameThr);
             setPlacingShip(player.ships['submarine']);
         } else if (placingShip.name === 'Submarine') {
-            strikeOutText(shipNameFour);
             setPlacingShip(player.ships['destroyer']);
         } else if (placingShip.name === 'Destroyer') {
-            strikeOutText(shipNameFive);
             setPlacingShip('done');
         }
     };
@@ -79,48 +70,37 @@ const PlacingPage = ({ player, setViewPage, vsCpu }) => {
     };
 
     const donePlacing = () => {
-        resetStrikeOutText();
         resetShipPlacing();
         setViewPage('placingTwo');
     };
 
     return (
         <div
-            className="h-screen bg-center"
+            className={classes.container}
             style={{
                 minHeight: '100vh',
                 backgroundImage: `url(${skull})`,
                 backgroundSize: '20%',
             }}
         >
-            <div className="text-center text-red-700 text-3xl pb-1 bg-gray-300 bg-opacity-90">
-                {player.name} Ships
-            </div>
+            <div className={classes.headerBar}>{player.name} Ships</div>
             <div
                 style={{ backgroundSize: '12.5% 12.5%', opacity: '.8' }}
-                className="h-4/6 w-5/6 mt-3 mx-auto bg-water-tile my-grid-border"
+                className={classes.boardGridContainer}
             >
                 <BoardGrid
                     boardState={player.board.boardState}
                     gridSpotClick={gridSpotClick}
                 ></BoardGrid>
             </div>
-            <div
-                className={`${
-                    placingShip === 'done' ? 'flex' : 'hidden'
-                } justify-center text-red-700`}
-            >
+            <div className={classes.doneButton(placingShip)}>
                 <StdButton
                     textSize="text-xl"
                     string="Ready"
                     handleClick={donePlacing}
                 ></StdButton>
             </div>
-            <div
-                className={`${
-                    placingShip !== 'done' ? 'flex' : 'hidden'
-                } justify-evenly text-red-700`}
-            >
+            <div className={classes.axisButtonsContainer(placingShip)}>
                 <StdButton
                     textSize={`${axis === 'X' ? 'text-4xl' : 'text-xl'}`}
                     string="X"
@@ -134,59 +114,18 @@ const PlacingPage = ({ player, setViewPage, vsCpu }) => {
                     Y
                 </StdButton>
             </div>
-            <div className="relative flex justify-center items-center">
+            <div className={classes.shipNamesContainer}>
                 <img
                     src={scroll}
                     alt="scroll-img"
                     className="w-full h-36"
                 ></img>
                 <div className="absolute w-44 sm:w-64 text-sm sm:text-lg flex flex-wrap justify-around">
-                    <div
-                        ref={shipNameOne}
-                        className={`mx-1 ${
-                            placingShip.name === 'Carrier' ? 'text-red-700' : ''
-                        }`}
-                    >
-                        Carrier
-                    </div>
-                    <div
-                        ref={shipNameTwo}
-                        className={`mx-1 ${
-                            placingShip.name === 'Battleship'
-                                ? 'text-red-700'
-                                : ''
-                        }`}
-                    >
-                        Battleship
-                    </div>
-                    <div
-                        ref={shipNameThr}
-                        className={`mx-1 ${
-                            placingShip.name === 'Cruiser' ? 'text-red-700' : ''
-                        }`}
-                    >
-                        Cruiser
-                    </div>
-                    <div
-                        ref={shipNameFour}
-                        className={`mx-1 ${
-                            placingShip.name === 'Submarine'
-                                ? 'text-red-700'
-                                : ''
-                        }`}
-                    >
-                        Submarine
-                    </div>
-                    <div
-                        ref={shipNameFive}
-                        className={`mx-1 ${
-                            placingShip.name === 'Destroyer'
-                                ? 'text-red-700'
-                                : ''
-                        }`}
-                    >
-                        Destroyer
-                    </div>
+                    <PlacingShipsNames
+                        player={player}
+                        placingShip={placingShip}
+                        setPlacingShip={setPlacingShip}
+                    ></PlacingShipsNames>
                 </div>
             </div>
         </div>
